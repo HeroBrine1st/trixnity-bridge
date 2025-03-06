19939c1017a8d6e53d4f800214edbb4595445018
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-03-06 22:27:39 +0300

    Remove unused method from RemoteActorId

--- bridge/src/commonMain/kotlin/ru/herobrine1st/matrix/bridge/api/value/RemoteActorId.kt
+++ bridge/src/commonMain/kotlin/ru/herobrine1st/matrix/bridge/api/value/RemoteActorId.kt
@@ -7,14 +7,4 @@ package ru.herobrine1st.matrix.bridge.api.value
  * users in room, it uses [RemoteActorId] of actor that has access to it to signal [ru.herobrine1st.matrix.bridge.api.RemoteWorker.getRoomMembers]
  * what actor to use. [ru.herobrine1st.matrix.bridge.api.RemoteWorker] may use or ignore it, depending on its capabilities and other internal state.
  */
-
-public interface RemoteActorId {
-    /**
-     * This function has same semantics as [toString] but result is used to build an alias.
-     *
-     * Resulting value MUST be unique for every actor.
-     *
-     * @return string that can be used as part of [room alias](https://spec.matrix.org/latest/appendices/#room-aliases)
-     */
-    public fun toAliasPart(): String
-}
+public interface RemoteActorId
